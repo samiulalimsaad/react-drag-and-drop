60ac63b783eda74d9391e4d5b3f65ae705fa8d13
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-21 01:07:59 +0600

    final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,18 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <title>Drag and Drop</title>
                 <meta name="description" content="Drag and Drop" />
                 <link rel="icon" href="/favicon.ico" />
+
+                <meta
+                    property="og:url"
+                    content="https://react-drag-and-drop-samiulalimsaad.vercel.app/"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="React Drag And Drop" />
+                <meta
+                    property="og:description"
+                    content="React Drag and Drop game for equation solver. Drag and Drop blocks and enter your guess number, compare it WIN!!"
+                />
+                <meta property="og:image" content="/public/Drag-and-Drop.png" />
             </Head>
             <Component {...pageProps} />
         </>
